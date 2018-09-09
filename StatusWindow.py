63e49bfef7ce15feea0686ff3b124a2884ec64b8
MikeTheWatchGuy <mike_barnett@hotmail.com>
2018-09-08 23:33:37 -0400

    Mikes Changes

--- StatusWindow.py
+++ StatusWindow.py
@@ -3,7 +3,7 @@ import PySimpleGUI as sg
 
 def status_window():
     sg.ChangeLookAndFeel('TealMono')  # Changes color scheme of window created
-    form = sg.FlexForm('Status Window', auto_size_text=True, auto_size_buttons=False, grab_anywhere=False)
+    form = sg.FlexForm('Status Window', auto_size_buttons=False, grab_anywhere=False, return_keyboard_events=True)
 
     start_class = sg.Text('', size=(12, 1), font=('Helvetica', 20), background_color='black', text_color='white',
                           justification='center')
@@ -23,38 +23,46 @@ def status_window():
               [sg.Text('Class:', size=(5, 1), font=('Helvetica', 20)), start_class, sg.RealtimeButton('Class Info')],
               [sg.Text(' ' * 34), sg.Exit()]]
 
-    form.LayoutAndRead(layout, non_blocking=True)
+    form.Layout(layout)
 
     while True:
-        button, values = form.ReadNonBlocking()
+        button, values = form.Read()
         if values is None or button == 'Exit': break  # Program ends successfully if 'Quit' is clicked or window is closed
 
         # When no stat requirements are met:
-        if all([int(values['STR']), int(values['INT']), int(values['DEX'])]) < 10: start_class.Update('')
+        try:
+            strength = int(values['STR'])
+            intel = int(values['INT'])
+            dex = int(values['DEX'])
+        except:
+            continue
+
+        if all((strength<10, intel<10, dex<10)): start_class.Update('')
 
         # Classes based on one stat:
-        if int(values['STR']) == 10:
+        if strength == 10:
             start_class.Update(class_list[0])
-        elif int(values['INT']) == 10:
-            start_class.Update(class_list[
-                                   1])  # Class is displayed in window when the stat requirements are met  (stat requirements are placeholder)
-        elif int(values['DEX']) == 10:
+        elif intel == 10:
+            start_class.Update(class_list[1])  # Class is displayed in window when the stat requirements are met  (stat requirements are placeholder)
+        elif dex == 10:
             start_class.Update(class_list[2])
 
         # Classes based on two stats:
-        if int(values['STR']) >= 10 and int(values['INT']) >= 10:
+        if strength >= 10 and intel >= 10:
             start_class.Update(class_list[3])
-        elif int(values['STR']) >= 10 and int(values['DEX']) >= 10:
+        elif strength >= 10 and dex >= 10:
             start_class.Update(class_list[4])
-        elif int(values['INT']) >= 10 and int(values['DEX']) >= 10:
+        elif intel >= 10 and dex >= 10:
             start_class.Update(class_list[5])
 
         # Classes based on three stats:
-        if int(values['STR']) >= 10 and int(values['INT']) >= 10 and int(values['DEX']) >= 10: start_class.Update(
-            class_list[6])
+        if strength >= 10 and intel >= 10 and dex >= 10: start_class.Update(class_list[6])
 
         # Class info button
         if button == 'Class Info' and start_class.DisplayText == '':
             sg.Popup('Not a Class:', 'If you see this, go back and obtain a class!')
         if button == 'Class Info' and start_class.DisplayText == 'Warrior':  # Example of a popup display explaining the class when the button is pressed
             sg.Popup('Warrior Class:', 'A class that specializes in melee combat')  # Placeholder descriptions
+
+
+status_window()
